d8cd1aee605ca17c6052eb40bc52a35e492dcdde
Ethan Arterberry <ethanarterberry@gmail.com>
2015-03-16 17:38:18 -0700

    fix addsnippet

--- quickie.js
+++ quickie.js
@@ -7,7 +7,10 @@ function getsnippet(code) {
 
 function addsnippet(code) {
   var js = "!function(e,a,t,n,c,o,s){e.GoogleAnalyticsObject=c,e[c]=e[c]||function(){(e[c].q=e[c].q||[]).push(arguments)},e[c].l=1*new Date,o=a.createElement(t),s=a.getElementsByTagName(t)[0],o.async=1,o.src=n,s.parentNode.insertBefore(o,s)}(window,document,\"script\",\"\/\/www.google-analytics.com\/analytics.js\",\"ga\"),ga(\"create\",\"" + code + "\",\"auto\"),ga(\"send\",\"pageview\");";
-  var snippet = "<script>" + '\n' + js + '\n' + "</script>";
+  var snippet = '\n' + js + '\n';
   
-  document.getElementsByTagName('head')[0].appendChild(snippet);
+  var script = document.createElement('script');
+  script.innerHTML = snippet;
+  
+  document.getElementsByTagName('head')[0].appendChild(script);
 }

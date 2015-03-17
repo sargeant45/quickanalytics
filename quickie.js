5f2d8ac2ad73d6da3c312448f6ef6319557810e3
Ethan Arterberry <ethanarterberry@gmail.com>
2015-03-16 17:42:25 -0700

    fix script

--- quickie.js
+++ quickie.js
@@ -1,12 +1,12 @@
 function getsnippet(code) {
-  var js = "!function(e,a,t,n,c,o,s){e.GoogleAnalyticsObject=c,e[c]=e[c]||function(){(e[c].q=e[c].q||[]).push(arguments)},e[c].l=1*new Date,o=a.createElement(t),s=a.getElementsByTagName(t)[0],o.async=1,o.src=n,s.parentNode.insertBefore(o,s)}(window,document,\"script\",\"\/\/www.google-analytics.com\/analytics.js\",\"ga\"),ga(\"create\",\"" + code + "\",\"auto\"),ga(\"send\",\"pageview\");";
+  var js = "(function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){\r\n  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\r\n  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\r\n  })(window,document,\'script\',\'\/\/www.google-analytics.com\/analytics.js\',\'ga\');\r\n\r\n  ga(\'create\', \'" + code + "\', \'auto\');\r\n  ga(\'send\', \'pageview\');";
   var snippet = "<script>" + '\n' + js + '\n' + "</script>";
   
   return snippet;
 }
 
 function addsnippet(code) {
-  var js = "!function(e,a,t,n,c,o,s){e.GoogleAnalyticsObject=c,e[c]=e[c]||function(){(e[c].q=e[c].q||[]).push(arguments)},e[c].l=1*new Date,o=a.createElement(t),s=a.getElementsByTagName(t)[0],o.async=1,o.src=n,s.parentNode.insertBefore(o,s)}(window,document,\"script\",\"\/\/www.google-analytics.com\/analytics.js\",\"ga\"),ga(\"create\",\"" + code + "\",\"auto\"),ga(\"send\",\"pageview\");";
+  var js = "(function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){\r\n  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\r\n  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\r\n  })(window,document,\'script\',\'\/\/www.google-analytics.com\/analytics.js\',\'ga\');\r\n\r\n  ga(\'create\', \'" + code + "\', \'auto\');\r\n  ga(\'send\', \'pageview\');";
   var snippet = '\n' + js + '\n';
   
   var script = document.createElement('script');

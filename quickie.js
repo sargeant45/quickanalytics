9c7e97f04989ef50a642699dcc21eadef6054e27
Ethan Arterberry <ethanarterberry@gmail.com>
2015-03-16 17:19:58 -0700

    Finish quickie.js

--- quickie.js
+++ quickie.js
@@ -1,3 +1,13 @@
-function analyticscode() {
+function getsnippet(code) {
+  var js = "!function(e,a,t,n,c,o,s){e.GoogleAnalyticsObject=c,e[c]=e[c]||function(){(e[c].q=e[c].q||[]).push(arguments)},e[c].l=1*new Date,o=a.createElement(t),s=a.getElementsByTagName(t)[0],o.async=1,o.src=n,s.parentNode.insertBefore(o,s)}(window,document,\"script\",\"\/\/www.google-analytics.com\/analytics.js\",\"ga\"),ga(\"create\",\"" + code + "\",\"auto\"),ga(\"send\",\"pageview\");";
+  var snippet = "<script>" + '\n' + js + '\n' + "</script>";
   
+  return snippet;
+}
+
+function addsnippet(code) {
+  var js = "!function(e,a,t,n,c,o,s){e.GoogleAnalyticsObject=c,e[c]=e[c]||function(){(e[c].q=e[c].q||[]).push(arguments)},e[c].l=1*new Date,o=a.createElement(t),s=a.getElementsByTagName(t)[0],o.async=1,o.src=n,s.parentNode.insertBefore(o,s)}(window,document,\"script\",\"\/\/www.google-analytics.com\/analytics.js\",\"ga\"),ga(\"create\",\"" + code + "\",\"auto\"),ga(\"send\",\"pageview\");";
+  var snippet = "<script>" + '\n' + js + '\n' + "</script>";
+  
+  document.getElementsByTagName('head')[0].appendChild(snippet);
 }

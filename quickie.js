598af254281b6bfe6466c08269a5af1ad868e202
Ethan Arterberry <ethanarterberry@gmail.com>
2015-03-16 17:43:07 -0700

    quickie

--- quickie.js
+++ quickie.js
@@ -4,13 +4,3 @@ function getsnippet(code) {
   
   return snippet;
 }
-
-function addsnippet(code) {
-  var js = "(function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){\r\n  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\r\n  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\r\n  })(window,document,\'script\',\'\/\/www.google-analytics.com\/analytics.js\',\'ga\');\r\n\r\n  ga(\'create\', \'" + code + "\', \'auto\');\r\n  ga(\'send\', \'pageview\');";
-  var snippet = '\n' + js + '\n';
-  
-  var script = document.createElement('script');
-  script.innerHTML = snippet;
-  
-  document.getElementsByTagName('head')[0].appendChild(script);
-}
